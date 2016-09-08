64f280e27ddf626cef38b36ba78670bdebda8b2d
XuyangZhao01 <Xuyangzhao01@gmail.com>
2016-09-08 11:43:42 -0700

    Added Files

---
+++ APLesson_01/ASCII_01.java
@@ -0,0 +1,8 @@
+public class ASCII_01
+{
+	public static void main (String[]args)
+	{
+		System.out.println ("         #o#\n       ####o#\n      #o# \\#|_#,#\n     ###\\ |/   #o#\n      # {}{      #\n         }{{\n        ,'  `");
+	
+	}
+}
---
+++ APLesson_01/ASCII_02.java
@@ -0,0 +1,18 @@
+public class ASCII_02
+{
+	public static void main (String[]args)
+	{
+		System.out.println ("  _______");
+		System.out.println (" /  12   \\");
+		System.out.println ("|    |    |");
+		System.out.println ("|9   |   3|");
+		System.out.println ("|     \\   |");
+		System.out.println ("|         |");
+		System.out.println ("\\___6___/");
+		System.out.println ("");
+		System.out.println ("");
+		System.out.println ("");
+		System.out.println ("");
+		
+	}
+}
