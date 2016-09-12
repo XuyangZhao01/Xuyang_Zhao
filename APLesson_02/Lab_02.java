b16d4be9fd6085a1f57e94237643e97f2b46f0c5
XuyangZhao01 <Xuyangzhao01@gmail.com>
2016-09-12 12:06:04 -0700

    Stuff
    New stuff

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,44 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a=2;
+		int b=4;
+		System.out.println("2 multiplied by 4 is " + a*b);
+		int c=5;
+		int d=4;
+		System.out.println("5 and 4 would produce "+ c*d);
+		int e=5;
+		int f =6;
+		System.out.println("5 multiplied by 6 is " + e*f);
+		String Name="Xuyang Zhao ";
+		String Address="5598 Valerio Trail ";
+		String City="San Diego ";
+		String ZIP="92130";
+		System.out.println(Name);
+		System.out.println(Address + City + ZIP);
+		System.out.println("THE SURFACE AREA OF A RECTANGLE");
+		int l=2;
+		int w=3;
+		int h=4;
+		int SA=2*l*w+2*l*h+2*h*w;
+		System.out.println("The Surface Area of your Rectangle is " + SA);
+		
+
+
+	}	
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+}
