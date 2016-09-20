4928baada43326ccec6872cce8cc97afc12d3b11
XuyangZhao01 <Xuyangzhao01@gmail.com>
2016-09-20 11:08:42 -0700

    Stuff
    Stuff

---
+++ APLesson_04/Lesson_04.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Lesson_04
+{
+		public static void main(String[]args)
+		{
+			System.out.printf("%10s   %10.2f", "test", 4564.3453);
+			
+		}
+		
+		public void format (String word, double number)
+		{
+			System.out.printf("\n%10s   %10.2f", word, number);
+		}
+		
+		
+} 
